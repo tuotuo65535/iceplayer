38e3bc2b5bea63878074d3152894c4b60da547fe
tuotuo65535 <562182858@qq.com>
2018-04-20 16:00:08 +0800

    推荐页组件

--- src/components/MyRecommend/MyRecommend.vue
+++ src/components/MyRecommend/MyRecommend.vue
@@ -1,14 +1,74 @@
 <!-- Created by chenzy on 2018/4/19. -->
+<!-- 推荐页组件 -->
 <template>
-    <div class="">
+    <div class="my-recommend" ref="recommendRef">
+        <!-- better-scroll 滚动组件，当请求到lists时才渲染 -->
+        <my-scroll ref="scroll" :data="lists" class="recommend-content">
+            <div>
+                <div v-if="recommends.length" class="slider-wrapper">
+                </div>
 
+                <!-- 歌单推荐列表 -->
+                <div class="recommend-list">
+                    <h1>热门歌单推荐</h1>
+                    <ul>
+                        <li>
+
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </my-scroll>
+
+        <router-view></router-view>
     </div>
 </template>
 
 <script>
-    export default {}
+    import { getRecommend, getList } from '@/api/recommend.js'
+    import MyScroll from '@/components/base/MyScroll/MyScroll'
+    export default {
+        components: {
+            MyScroll
+        },
+        data () {
+            return {
+                recommends: [],
+                lists: []
+            }
+        },
+        created () {
+            this._getRecommend()
+        },
+        methods: {
+            _getRecommend () {
+                getRecommend().then((res) => {
+                    if (res.code === 0) {
+                        this.recommends = res.data.slider
+                    }
+                })
+            }
+        }
+    }
 </script>
 
 <style lang="scss" scoped>
+    @import "~@/common/scss/const.scss";
+    @import "~@/common/scss/mymixin.scss";
 
+    .my-recommend {
+        position: fixed;
+        width: 100%;
+        top: 88px;
+        bottom: 0;
+        .recommend-content {
+            height: 100%;
+            overflow: hidden;
+            .slide-wrapper {
+                position: relative;
+                width: 100%;
+                overflow: hidden;
+            }
+        }
+    }
 </style>
---
+++ src/components/base/MyScroll/MyScroll.vue
@@ -0,0 +1,54 @@
+<!-- Created by chenzy on 2018/4/20. -->
+<!-- 滚动组件 -->
+<template>
+    <div ref="wrapper" class="my-scroll">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    import BScroll from 'better-scroll'
+    export default {
+        components: {},
+        data() {
+            return {}
+        },
+        props: {
+            //传入的数据
+            data: {
+                type: Array,
+                default: null
+            },
+            probeType: {
+                type: Number,
+                default: 1
+            },
+            click: {
+                type: Boolean,
+                default: true
+            }
+        },
+        methods: {
+            _initScroll() {
+                if (!this.$refs.wrapper) {
+                    return
+                }
+
+                this.scroll = new BScroll(this.$refs.wrapper, {
+                    probeType: this.probeType,
+                    click: this.click
+                })
+            }
+        },
+        created () {},
+        mounted () {
+            setTimeout(() => {
+                this._initScroll()
+            }, 20)
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
