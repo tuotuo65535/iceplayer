bb070b9b466745934efa8677d9301f8b1124fb41
tuotuo65535 <562182858@qq.com>
2018-05-02 15:14:43 +0800

    歌手组件初始化

--- src/components/MySinger/MySinger.vue
+++ src/components/MySinger/MySinger.vue
@@ -1,12 +1,41 @@
 <!-- Created by chenzy on 2018/4/27. -->
+<!-- 歌手页组件 -->
 <template>
-    <div class="">
-
+    <div class="my-singer" ref="singerRef">
+        <my-phone-list ref="listRef" style="" :data="singers"></my-phone-list>
+        <router-view></router-view>
     </div>
 </template>
 
 <script>
-    export default {}
+    import MyPhoneList from '@/components/base/MyPhoneList/MyPhoneList'
+    import {playlistMixin} from '@/common/js/mixin.js'
+    export default {
+        mixins: [playlistMixin],
+        components: {
+            MyPhoneList
+        },
+        data () {
+            return {
+                singers: []
+            }
+        },
+        props: {},
+        watch: {},
+        methods: {
+            // 当有迷你播放器时，调整滚动底部距离
+            handlePlaylist(playlist) {
+                let bottom = playlist.length > 0 ? '60px' : ''
+                this.$refs.singerRef.style.bottom = bottom
+                this.$refs.listRef.refresh()
+            }
+        },
+        created() {
+
+        },
+        mounted() {},
+        destroyed() {}
+    }
 </script>
 
 <style lang="scss" scoped>
---
+++ src/components/base/MyPhoneList/MyPhoneList.vue
@@ -0,0 +1,77 @@
+<!-- Created by chenzy on 2018/5/2. -->
+<!-- 歌手列表 -->
+<template>
+    <my-scroll ref="scrollRef">
+        <ul>
+            <!-- 左侧歌手列表 -->
+            <li ref="leftRef" v-for="group in data" class="list-group">
+                <h2 class="list-group-title">{{ group.title }}</h2>
+                <ul>
+                    <li v-for="item in group.items" class="list-group-item">
+                        <img v-lazy="isTemplateLike.avatar" class="avatar">
+                        <span class="name">{{ item.name }}</span>
+                    </li>
+                </ul>
+                <!-- 右侧字母列表 -->
+                <div class="list-shortcut">
+                    <ul>
+                        <li v-for="(item, index) in shortcut">
+                            {{ item }}
+                        </li>
+                    </ul>
+                </div>
+                <!-- 滚动固定标题实现 -->
+                <div ref="fixedTitleRef" v-show="fixedTitle" class="list-fixed">
+                    <h1 class="fixed-title">{{ fixedTitle }}</h1>
+                </div>
+            </li>
+        </ul>
+    </my-scroll>
+</template>
+
+<script>
+    import MyScroll from '@/components/base/MyScroll/MyScroll'
+    import {myDOM} from '@/common/js/myutils'
+    export default {
+        components: {
+            MyScroll
+        },
+        data() {
+            return {
+                scrollY: -1,
+                currentIndex: 0,
+                // 标题上推y值（热门标题 - A 标题）
+                diff: -1
+            }
+        },
+        props: {
+            data: {
+                type: Array,
+                default: []
+            }
+        },
+        watch: {
+            data() {
+
+            }
+        },
+        methods: {
+            // 对父元素提供的刷新 better-scroll方法
+            refresh() {
+                this.$refs.scrollRef.refresh
+            }
+        },
+        computed: {
+
+        },
+        created() {
+
+        },
+        mounted() {},
+        destroyed() {}
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
