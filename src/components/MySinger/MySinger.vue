ed073f00feeebbc2f7a7036215abcf71007ddce1
tuotuo65535 <562182858@qq.com>
2018-05-04 11:13:37 +0800

    展示歌手列表和字母导航

--- src/components/MySinger/MySinger.vue
+++ src/components/MySinger/MySinger.vue
@@ -9,8 +9,12 @@
 
 <script>
     import { getSingerList } from '@/api/singer.js'
+    import { createSinger } from '@/common/js/SingerClass.js'
     import MyPhoneList from '@/components/base/MyPhoneList/MyPhoneList'
     import {playlistMixin} from '@/common/js/mixin.js'
+
+    const HOT_TITLE = '热门'
+    const HOT_NUM = 10
     export default {
         mixins: [playlistMixin],
         components: {
@@ -32,11 +36,59 @@
             },
             _getSingerList() {
                 getSingerList().then(res => {
-                    console.log(res)
                     if (res.code === 0 ){
-                        //this.singers = this._formatSingers(res.data.list)
+                        this.singers = this._formatSingers(res.data.list)
                     }
                 })
+            },
+            // 重组list数据
+            _formatSingers (list) {
+                //热门歌手
+                let map = {
+                    hot: {
+                        title: HOT_TITLE,
+                        items: []
+                    }
+                }
+
+                // 填充歌手数据
+                list.forEach((item, index) => {
+                    //填充热门歌手数据
+                    if (index < HOT_NUM) {
+                        map.hot.items.push(createSinger(item))
+                    }
+
+                    // 填充 a-z 字母
+                    let key = item.Findex
+                    if (!map[key]){
+                        map[key] = {
+                            title: key,
+                            items: []
+                        }
+                    }
+
+                    // 填充对应字母歌手数据
+                    map[key].items.push(createSinger(item))
+                })
+
+                // 得到有序列表
+                let hot = []
+                let others = []
+
+                for (let key in map) {
+                    let value = map[key]
+                    if (value.title.match(/[a-zA-Z]/)){
+                        others.push(value)
+                    } else if (value.title === HOT_TITLE) {
+                        hot.push(value)
+                    }
+                }
+
+                others.sort((a, b) => {
+                    return a.title.charCodeAt(0) - b.title.charCodeAt(0)
+                })
+
+                return hot.concat(others)
             }
         },
         created() {
@@ -48,5 +100,13 @@
 </script>
 
 <style lang="scss" scoped>
+    @import '~@/common/scss/const.scss';
+    @import '~@/common/scss/mymixin.scss';
 
+    .my-singer {
+        position: fixed;
+        top: 88px;
+        bottom: 0;
+        width: 100%;
+    }
 </style>
--- src/components/base/MyPhoneList/MyPhoneList.vue
+++ src/components/base/MyPhoneList/MyPhoneList.vue
@@ -1,31 +1,37 @@
 <!-- Created by chenzy on 2018/5/2. -->
 <!-- 歌手列表 -->
 <template>
-    <my-scroll ref="scrollRef">
+    <my-scroll ref="scrollRef"
+                :data="data"
+                class="my-phone-list">
         <ul>
             <!-- 左侧歌手列表 -->
             <li ref="leftRef" v-for="group in data" class="list-group">
                 <h2 class="list-group-title">{{ group.title }}</h2>
                 <ul>
                     <li v-for="item in group.items" class="list-group-item">
-                        <img v-lazy="isTemplateLike.avatar" class="avatar">
+                        <img v-lazy="item.avatar" class="avatar">
                         <span class="name">{{ item.name }}</span>
                     </li>
                 </ul>
-                <!-- 右侧字母列表 -->
-                <div class="list-shortcut">
-                    <ul>
-                        <li v-for="(item, index) in shortcut">
-                            {{ item }}
-                        </li>
-                    </ul>
-                </div>
-                <!-- 滚动固定标题实现 -->
-                <div ref="fixedTitleRef" v-show="fixedTitle" class="list-fixed">
-                    <h1 class="fixed-title">{{ fixedTitle }}</h1>
-                </div>
             </li>
         </ul>
+
+        <!-- 右侧字母列表 -->
+        <div class="list-shortcut">
+            <ul>
+                <li v-for="(item, index) in shortcut"
+                    :data-index="index"
+                    :class="{ 'current' : currentIndex === index}"
+                    class="item">
+                    {{ item }}
+                </li>
+            </ul>
+        </div>
+        <!-- 滚动固定标题实现 -->
+        <div ref="fixedTitleRef" v-show="fixedTitle" class="list-fixed">
+            <h1 class="fixed-title">{{ fixedTitle }}</h1>
+        </div>
     </my-scroll>
 </template>
 
@@ -62,7 +68,18 @@
             }
         },
         computed: {
-
+            // 快速入口排列数组
+            shortcut() {
+                return this.data.map(group => {
+                    return group.title.substr(0, 1)
+                })
+            },
+            fixedTitle() {
+                if (this.scrollY > 0) {
+                    return ''
+                }
+                return this.data[this.currentIndex] ? this.data[this.currentIndex].title: ''
+            }
         },
         created() {
 
@@ -73,5 +90,82 @@
 </script>
 
 <style lang="scss" scoped>
+    @import '~@/common/scss/const.scss';
+    @import '~@/common/scss/mymixin.scss';
 
+    .my-phone-list {
+        position: relative;
+        width: 100%;
+        height: 100%;
+        overflow: hidden;
+        background: $color-background;
+        .list-group {
+            padding-bottom: 30px;
+            .list-group-title {
+                height: 30px;
+                line-height: 30px;
+                padding-left: 20px;
+                font-size: $font-size-small;
+                color: $color-text-l;
+                background: $color-highlight-background;
+            }
+            .list-group-item {
+                display: flex;
+                align-items: center;
+                padding: 20px 0 0 30px;
+                .avatar {
+                    width: 50px;
+                    height: 50px;
+                    border-radius: 50%;
+                }
+                .name {
+                    margin-left: 20px;
+                    color: $color-text-l;
+                    font-size: $font-size-medium;
+                }
+            }
+        }
+        .list-shortcut {
+            position: absolute;
+            z-index: 30;
+            right: 0;
+            top: 50%;
+            transform: translateY(-50%);
+            width: 20px;
+            padding: 20px 0;
+            border-radius: 10px;
+            text-align: center;
+            background: $color-background-d;
+            font-family: Helvetica;
+            .item {
+                padding: 3px;
+                line-height: 1;
+                color: $color-text-l;
+                font-size: $font-size-small;
+                &.current {
+                    color: $color-theme;
+                }
+            }
+        }
+        .list-fixed {
+            position: absolute;
+            top: -1px;
+            left: 0;
+            width: 100%;
+            .fixed-title {
+                height: 30px;
+                line-height: 30px;
+                padding-left: 20px;
+                font-size: $font-size-small;
+                color: $color-text-l;
+                background: $color-highlight-background;
+            }
+        }
+        .loading-container {
+            position: absolute;
+            width: 100%;
+            top: 50%;
+            transform: translateY(-50%);
+        }
+    }
 </style>
