c6ec2455a09dd89f4c852f618a4342272b79a3f6
tuotuo65535 <562182858@qq.com>
2018-05-08 15:20:37 +0800

    搜索模块

--- src/components/MySearch/MySearch.vue
+++ src/components/MySearch/MySearch.vue
@@ -1,14 +1,183 @@
-<!-- Created by chenzy on 2018/4/19. -->
+<!-- Created by chenzy on 2018/5/8. -->
+<!-- 搜索页组件 -->
 <template>
-    <div class="">
+    <div class="my-search">
+        <!-- 搜索框 -->
+        <div class="search-box-wrapper">
+            <my-search-box ref="searchBoxRef"></my-search-box>
+        </div>
 
+        <div class="shortcut-wrapper" v-show="!query" ref="shortcutRef">
+            <my-scroll class="shortcut" ref="scrollRef"
+                       :data="scrollData" :refreshDelay="refreshDelay">
+                <div>
+                    <!-- 热门搜索 -->
+                    <div class="hot-key">
+                        <h1 class="title">热门搜索</h1>
+                        <ul>
+                            <li class="item" v-for="item in hotkey">
+                                <span>{{item.k}}</span>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <!-- 搜素历史 -->
+                    <div class="search-history" v-show="searchHistory.length">
+                        <h1 class="title">
+                            <span class="text">搜索历史</span>
+                            <span class="clear">
+                                <i class="icon-clear"></i>
+                            </span>
+                        </h1>
+
+                        <my-search-list :searches="searchHistory"></my-search-list>
+                    </div>
+                </div>
+            </my-scroll>
+        </div>
+
+        <!-- 搜索结果 -->
+        <div class="search-result" ref="resultRef" v-show="query">
+            <my-suggest-List ref="suggestRef"></my-suggest-List>
+        </div>
+
+        <!-- 清空弹窗 -->
+        <my-confirm ref="confirmRef"></my-confirm>
+
+        <router-view></router-view>
     </div>
 </template>
 
 <script>
-    export default {}
+    import MySearchBox from '@/components/base/MySearchBox/MySearchBox'
+    import MySuggestList from '@/components/MySuggestList/MySuggestList'
+    import MySearchList from '@/components/base/MySearchList/MySearchList'
+    import MyConfirm from '@/components/base/MyConfirm/MyConfirm'
+    import MyScroll from '@/components/base/MyScroll/MyScroll'
+    import { getHotKey } from '@/api/search.js'
+    import { mapActions, mapGetters } from 'vuex'
+    import { playlistMixin } from '@/common/js/mixin.js'
+
+    export default {
+        mixins: [playlistMixin],
+        components: {
+            MySearchBox,
+            MySuggestList,
+            MySearchList,
+            MyConfirm,
+            MyScroll
+        },
+        data() {
+            return {
+                // 热门搜索关键字
+                hotkey: [],
+                // 搜索字段
+                query: '',
+                // 是否显示歌手
+                zhida: true,
+                refreshDelay: 100
+            }
+        },
+        methods: {
+            ...mapActions(['saveHistory', 'delHistory', 'clearHistory']),
+            // 获取热搜索数据
+            _getHotKey() {
+                getHotKey().then(res => {
+                    if (res.code === 0) {
+                        this.hotkey= res.data.hotkey.slice(0,10)
+                    }
+                })
+            },
+            // 当有迷你播放器时，调整滚动底部距离
+            handlePlaylist(playlist) {
+                let bottom = playlist.length > 0 ? '60px' : ''
+
+                this.$refs.shortcutRef.style.bottom = bottom
+                this.$refs.scrollRef.refresh()
+
+                this.$refs.resultRef.style.bottom = bottom
+                this.$refs.suggestRef.refresh()
+            }
+        },
+        computed: {
+            ...mapGetters(['searchHistory']),
+            scrollData() {
+                return this.hotkey.concat(this.searchHistory)
+            }
+        },
+        created() {
+            this._getHotKey()
+        }
+    }
 </script>
 
 <style lang="scss" scoped>
+    @import '~@/common/scss/const.scss';
+    @import '~@/common/scss/mymixin.scss';
 
+    .my-search {
+        .search-box-wrapper {
+            margin: 20px;
+        }
+        .shortcut-wrapper {
+            position: fixed;
+            top: 178px;
+            bottom: 0;
+            width: 100%;
+            .shortcut {
+                height: 100%;
+                overflow: hidden;
+                .hot-key {
+                    // height: 170px;
+                    // overflow: hidden;
+                    margin: 0 20px 20px 20px;
+                    .title {
+                        margin-bottom: 20px;
+                        font-size: $font-size-medium;
+                        color: $color-text-l;
+                    }
+                    .item {
+                        display: inline-block;
+                        padding: 5px 10px;
+                        margin: 0 20px 10px 0;
+                        border-radius: 6px;
+                        background: $color-highlight-background;
+                        font-size: $font-size-medium;
+                        color: $color-text-d;
+                        &.special {
+                            color: rgba(255, 255, 255, 0.7);
+                            // border: 1px solid #fc4524;
+                        }
+                    }
+                }
+                .search-history {
+                    position: relative;
+                    margin: 0 20px;
+                    .title {
+                        display: flex;
+                        align-items: center;
+                        height: 40px;
+                        font-size: $font-size-medium;
+                        color: $color-text-l;
+                        .text {
+                            flex: 1;
+                        }
+                        .clear {
+                            @include extend-click();
+                            .icon-clear {
+                                font-size: $font-size-medium;
+                                color: $color-text-d;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        .search-result {
+            position: fixed;
+            width: 100%;
+            top: 178px;
+            bottom: 0;
+        }
+    }
 </style>
--- src/components/MySuggestList/MySuggestList.vue
+++ src/components/MySuggestList/MySuggestList.vue
@@ -1,14 +1,134 @@
 <!-- Created by chenzy on 2018/4/19. -->
+<!-- 搜索建议页列表组件 -->
 <template>
-    <div class="">
+    <my-scroll class="my-suggest-list"
+               ref="scrollRef"
+               :data="result"
+               :beforeScroll="beforeScrollData"
+               :pullup="pullup">
+        <ul class="suggest-list">
+            <li class="suggest-item" v-for="item in result">
+                <div class="icon">
+                    <i :class="icon"></i>
+                </div>
 
-    </div>
+                <div class="name">
+                    <p class="text" v-html="getSingernameOrSongname(item)"></p>
+                </div>
+            </li>
+
+            <MyLoading v-show="hasMore" title=""></MyLoading>
+        </ul>
+
+        <div class="no-result-wrapper" v-if="!hasMore && !result.length">
+            <my-no-result></my-no-result>
+        </div>
+    </my-scroll>
 </template>
 
 <script>
-    export default {}
+    import MyScroll from '@/components/base/MyScroll/MyScroll'
+    import MyLoading from '@/components/base/MyLoading/MyLoading'
+    import { search } from '@/api/search.js'
+    import { createSingerSong } from '@/common/js/SingerSongClass.js'
+    import { Singer } from '@/common/js/SingerClass.js'
+    import { mapMutations, mapActions } from 'vuex'
+
+    const TYPE_SINGER = 'singer'
+
+    export default {
+        components: {
+            MyScroll,
+            MyLoading
+        },
+        data() {
+            return {
+                // 当前检索页数，用于下拉加载
+                page: 1,
+                // 每一页的数量
+                perpage: 20,
+                // 接受检索结果
+                result: [],
+                // 标识位，是否加载完
+                hasMore: true,
+                beforeScrollData: true,
+                pullup: true
+
+            }
+        },
+        props: {
+            // 接受的检索值
+            query: {
+                type: String,
+                default: ''
+            },
+            // 是否显示歌手
+            zhida: {
+                type: Boolean,
+                default: true
+            }
+        },
+        watch: {
+            query(newVal) {
+                this.search()
+            }
+        },
+        filters: {},
+        methods: {
+            // vuex
+            ...mapMutations({
+                'setSinger': 'SET_SINGER'
+            }),
+            ...mapActions(['inserSong']),
+            // parent call
+            refresh() {
+                this.$refs.scrollRef.refresh()
+            }
+        },
+        computed: {},
+        created () {},
+        mounted () {},
+        destroyed () {}
+    }
 </script>
 
 <style lang="scss" scoped>
+    @import '~@/common/scss/const.scss';
+    @import '~@/common/scss/mymixin.scss';
 
+    .my-suggest-list {
+        height: 100%;
+        overflow: hidden;
+        .suggest-list {
+            padding: 0 30px;
+            .suggest-item {
+                display: flex;
+                align-items: center;
+                padding-bottom: 20px;
+            }
+            .icon {
+                flex: 0 0 30px;
+                width: 30px;
+                [class^="icon-"] {
+                    font-size: 14px;
+                    color: $color-text-d;
+                }
+            }
+            .name {
+                flex: 1;
+                font-size: $font-size-medium;
+                color: $color-text-d;
+                overflow: hidden;
+                .text {
+                    @include no-wrap();
+                }
+            }
+        }
+        .no-result-wrapper {
+            position: absolute;
+            width: 100%;
+            top: 50%;
+            transform: translateY(-50%);
+        }
+    }
 </style>
